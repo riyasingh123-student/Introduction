eef81fd75a6f3be316bc7bb13feff4095fe183ff
Riya Singh <riyasingh790322@gmail.com>
2026-07-20 14:28:42 +0530

    Add files via upload

---
+++ CHAPTER 2 - PS/02_Problem2.py
@@ -0,0 +1,5 @@
+a = 37
+
+b = 5
+
+print("Remainder when a number is divided by", a % b)
---
+++ CHAPTER 2 - PS/06_Problem6.py
@@ -0,0 +1,5 @@
+a = int(input("Enter your number "))
+
+print("The square of two number is",a**2)
+print("The square of two number is",a*a)
+print("The square of two number is",a^2) # incorrect the finding square of a number in Python
