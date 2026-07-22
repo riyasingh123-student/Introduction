126c00aa9c6805f0c70903749dab387e298df52e
Riya Singh <riyasingh790322@gmail.com>
2026-07-22 13:14:26 +0530

    Add files via upload

---
+++ Chapter 4 - PS/01_problem1.py
@@ -0,0 +1,18 @@
+fruits = []
+
+f1 = input("Enter Fruit name: ")
+fruits.append(f1)
+f2 = input("Enter Fruit name: ")
+fruits.append(f2)
+f3 = input("Enter Fruit name: ")
+fruits.append(f3)
+f4 = input("Enter Fruit name: ")
+fruits.append(f4)
+f5 = input("Enter Fruit name: ")
+fruits.append(f1)
+f6 = input("Enter Fruit name: ")
+fruits.append(f1)
+f7 = input("Enter Fruit name: ")
+fruits.append(f1)
+
+print(fruits)
---
+++ Chapter 4 - PS/02_problem2.py
@@ -0,0 +1,17 @@
+marks = []
+
+f1 = int(input("Enter Marks here: "))
+marks.append(f1)
+f2 = int(input("Enter Marks here: "))
+marks.append(f2)
+f3 = int(input("Enter Marks here: "))
+marks.append(f3)
+f4 = int(input("Enter Marks here: "))
+marks.append(f4)
+f5 = int(input("Enter Marks here: "))
+marks.append(f1)
+f6 = int(input("Enter Marks here: "))
+marks.append(f1)
+
+marks.sort()
+print(marks)
