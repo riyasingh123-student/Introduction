d7ee870fda9f63dcbf7d42bf5c34365eb6b8dd87
Riya Singh <riyasingh790322@gmail.com>
2026-07-22 13:10:19 +0530

    Add files via upload

---
+++ Chapter 4/03_tuple.py
@@ -0,0 +1,3 @@
+a = (1,45,543,4578,False, "Jyoti", "Shivam")
+print(a)
+print(type(a))
---
+++ Chapter 4/04_tuple_methods.py
@@ -0,0 +1,10 @@
+a = (1,45,543,4578,False,45, "Jyoti", "Shivam")
+print(a)
+
+no = a.count(45)
+print(no)
+
+i = a.index(4578)
+print(i)
+
+print(len(a))
