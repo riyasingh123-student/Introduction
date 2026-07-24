91a22400947ff77a60c8ce02dd6a0c250be580bb
Riya Singh <riyasingh790322@gmail.com>
2026-07-24 13:53:27 +0530

    Add files via upload

---
+++ Chapter 6 - PS/04_problem4.py
@@ -0,0 +1,6 @@
+username = input("Enter username: ")
+
+if(len(username)<10):
+    print("Your username contain less than 10 characters")
+else:
+    print("Your username more than or equal to 10 characters")
